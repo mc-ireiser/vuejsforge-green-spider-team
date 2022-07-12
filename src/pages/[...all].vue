65bdc35b031f8712bd57f22aa735b72a10d1ef64
Jesus Rodriguez <mc.ireiser@gmail.com>
2022-07-11 23:26:31 -0400

    Add catch all route

---
+++ src/pages/[...all].vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>{{ $options.name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface Idata {
+  ok: boolean;
+}
+
+export default defineComponent({
+  name: "page-404",
+
+  data(): Idata {
+    return {
+      ok: true,
+    };
+  },
+
+  mounted() {
+    console.log(this.$options.name);
+  },
+
+  computed: {},
+
+  methods: {},
+});
+</script>
