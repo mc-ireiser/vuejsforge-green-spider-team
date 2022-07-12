199b011968a75d9573020df2db639db8d3518ec4
Jesus Rodriguez <mc.ireiser@gmail.com>
2022-07-12 13:07:22 -0400

    Add base boards page

--- src/pages/boards/index.vue
+++ src/pages/boards/index.vue
@@ -1,25 +1,69 @@
 <template>
   <div>
     <p>{{ $options.name }}</p>
+    <kbutton
+      class="mt-4"
+      :icon="'add'"
+      :fill-mode="'flat'"
+      @click="toggleDialog"
+      >Add new board</kbutton
+    >
+
+    <br /><br />
     <AppButton class="block">
-      <router-link to="/boards/1">Board 1</router-link>
+      <router-link to="/boards/1">go to Board 1</router-link>
     </AppButton>
+
+    <window
+      v-if="visible"
+      :title="'Status'"
+      @close="toggleDialog"
+      :initial-height="350"
+    >
+      <form class="k-form">
+        <fieldset>
+          <legend>Board Details</legend>
+
+          <label class="k-form-field">
+            <span>Title</span>
+            <kinput placeholder="Board title" />
+          </label>
+          <label class="k-form-field">
+            <span>Description</span>
+            <kinput placeholder="Board description" />
+          </label>
+        </fieldset>
+
+        <div class="text-right mt-6">
+          <kbutton @click="toggleDialog">Cancel</kbutton>
+          <span class="mr-4"></span>
+          <kbutton :theme-color="'primary'" @click="toggleDialog"
+            >Submit</kbutton
+          >
+        </div>
+      </form>
+    </window>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
-interface Idata {
-  ok: boolean;
-}
+import { Button } from "@progress/kendo-vue-buttons";
+import { Window } from "@progress/kendo-vue-dialogs";
+import { Input } from "@progress/kendo-vue-inputs";
 
 export default defineComponent({
   name: "BoardsPage",
 
-  data(): Idata {
+  components: {
+    kbutton: Button,
+    window: Window,
+    kinput: Input,
+  },
+
+  data() {
     return {
-      ok: true,
+      visible: false,
     };
   },
 
@@ -29,6 +73,10 @@ export default defineComponent({
 
   computed: {},
 
-  methods: {},
+  methods: {
+    toggleDialog() {
+      this.visible = !this.visible;
+    },
+  },
 });
 </script>
