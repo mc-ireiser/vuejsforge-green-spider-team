e4d05dc6f329d597be0123695c43ef84ffff6063
Jesus Rodriguez <mc.ireiser@gmail.com>
2022-07-11 23:07:45 -0400

    Config base routes

---
+++ src/pages/boards/[boardId]/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{ $options.name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+interface Idata {
+  ok: boolean;
+}
+
+export default {
+  name: "BoardPage",
+
+  data(): Idata {
+    return {
+      ok: true,
+    };
+  },
+
+  mounted() {
+    console.log(this.$options.name);
+  },
+
+  computed: {},
+
+  methods: {},
+};
+</script>
