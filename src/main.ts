20e2aa26f750772e9ffdf3c56cfa694ea58cba39
Jesus Rodriguez <mc.ireiser@gmail.com>
2022-07-13 19:12:44 -0400

    Initialize Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/base.css";
 import "@progress/kendo-theme-default/dist/all.css";
+// import "@progress/kendo-theme-default/dist/default-turquoise.scss";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(createPinia());
 
 app.mount("#app");
