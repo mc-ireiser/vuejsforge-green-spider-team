77ec79375ecb4b289c8f48b46c487ab7db1aa49f
Jesus Rodriguez <mc.ireiser@gmail.com>
2022-07-13 21:46:30 -0400

    Prepare view for show boards columns

--- src/pages/boards/[boardId]/index.vue
+++ src/pages/boards/[boardId]/index.vue
@@ -1,34 +1,39 @@
 <template>
   <div>
-    <p>{{ $options.name }}</p>
+    <p>{{ board.title }}</p>
+
+    <BoardCreateColumn></BoardCreateColumn>
+    <br />
+    <BoardColumn></BoardColumn>
+
+    <br />
     <AppButton class="block">
-      <router-link to="/boards/1/tasks/1">Task 1</router-link>
+      <router-link to="/boards/1/tasks/1">go to Task 1</router-link>
     </AppButton>
   </div>
 </template>
 
 <script lang="ts">
+import type { Board } from "@/types/index";
 import { defineComponent } from "vue";
-
-interface Idata {
-  ok: boolean;
-}
 
 export default defineComponent({
   name: "BoardPage",
-
-  data(): Idata {
+  data() {
     return {
-      ok: true,
+      board: {} as Board,
     };
   },
-
   mounted() {
     console.log(this.$options.name);
+    const boards = JSON.parse(localStorage.getItem("boards") || "[]");
+    if (boards instanceof Array) {
+      this.board = boards.filter(
+        (item) => item.id === this.$route.params.boardId
+      )[0];
+    }
   },
-
   computed: {},
-
   methods: {},
 });
 </script>
