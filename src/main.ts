1706c95cb5fbabe72b45c4aa15ae281b6ad9af71
Jesus Rodriguez <mc.ireiser@gmail.com>
2022-07-14 14:44:40 -0400

    Change theme

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/base.css";
-import "@progress/kendo-theme-default/dist/all.css";
-// import "@progress/kendo-theme-default/dist/default-turquoise.scss";
+// import "@progress/kendo-theme-default/dist/all.css";
+import "@progress/kendo-theme-default/dist/default-turquoise.scss";
 
 const app = createApp(App);
 
